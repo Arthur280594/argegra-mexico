838ef1809f82c0ff22c084868a4a63908c0f39ae
Arthur <59483473+Arthur280594@users.noreply.github.com>
2020-09-30 20:15:45 -0500

    crud

--- src/app/pages/familia/familia.component.ts
+++ src/app/pages/familia/familia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/services/crud/crud.service';
 
 @Component({
   selector: 'app-familia',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FamiliaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public crud: CrudService) { }
+
+  response:     any   = {};
+  data:       any[]   = [];
 
   ngOnInit(): void {
+
+    this.crud.get("familia").then(m => {
+      this.response = m;
+      console.log(this.response);
+      if(this.response.success){
+        this.data = this.response.data;
+      }
+    })
+
   }
 
 }
