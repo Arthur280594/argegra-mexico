90c26111ee615573013b25ece4c72c567a696fdc
Luis Arturo Chavez Mendoza <luis.arturo@minimalist.mx>
2020-12-07 19:32:11 -0600

    correcciones

--- src/app/pages/padron-acuicola/padron-acuicola.component.ts
+++ src/app/pages/padron-acuicola/padron-acuicola.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogPadronAcuicolaComponent } from 'src/app/dialog/dialog-padron-acuicola/dialog-padron-acuicola.component';
+import { CrudService } from 'src/app/services/crud/crud.service';
 
 @Component({
   selector: 'app-padron-acuicola',
@@ -9,9 +10,19 @@ import { DialogPadronAcuicolaComponent } from 'src/app/dialog/dialog-padron-acui
 })
 export class PadronAcuicolaComponent implements OnInit {
 
-  constructor(public _dialog: MatDialog,) { }
+  constructor(public _dialog: MatDialog,
+              public service: CrudService) { }
 
+  data: any [];
   ngOnInit(): void {
+    this.service.get('getPadronAcuicola').then(r => {
+      console.log(r);
+      let a:any = r;
+      if(a.success){
+        this.data = a.data;
+      }
+      
+    })
   }
 
   opendialog(){
