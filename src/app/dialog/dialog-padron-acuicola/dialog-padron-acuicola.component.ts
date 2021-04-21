6ea23f725a51029213a13ce9e7f83f4475635205
yess94 <67288198+yess94@users.noreply.github.com>
2021-04-20 21:05:25 -0500

    padron_acuicola

--- src/app/dialog/dialog-padron-acuicola/dialog-padron-acuicola.component.ts
+++ src/app/dialog/dialog-padron-acuicola/dialog-padron-acuicola.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CrudService } from 'src/app/services/crud/crud.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-dialog-padron-acuicola',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogPadronAcuicolaComponent implements OnInit {
 
-  constructor() { }
+  constructor( public dialogRef: MatDialogRef < any > ,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public _services: CrudService) { }
+    response:any;
+    especie:any []=[];
+    entidad_federativa:any []=[];
+    familia:any []=[];
 
-  ngOnInit(): void {
+ngOnInit(): void {
+console.log(this.data);
+this._services.get("especie").then(m => {
+        
+  this.response = m; 
+  console.log(this.response);
+  if(this.response.success){
+    this.especie = this.response.data;
   }
+})
 
+this._services.get("entidad_federativa").then(m => {
+
+  this.response = m;
+  console.log(this.response);
+  if(this.response.success){
+    this.entidad_federativa = this.response.data;
+  }
+})
+this._services.get("familia").then(m => {
+  this.response = m;
+  console.log(this.response);
+  if(this.response.success){
+    this.familia = this.response.data;
+  }
+})
+  }
+  guardar(){
+    this._services.post('post_padron_acuicola',this.data).then(r=>{
+      console.log(r);
+      let response : any = r;
+      if(response.success){
+        Swal.fire({
+          icon: "success",
+          title: 'Exito',
+          text: response.message
+        })
+        this.dialogRef.close();
+      }else{
+        Swal.fire({
+          icon: "error",
+          title: 'Error',
+          text: response.message
+        })
+      }
+    })
+  }
 }
+
--- src/app/pages/padron-acuicola/padron-acuicola.component.ts
+++ src/app/pages/padron-acuicola/padron-acuicola.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { FilterPipe } from 'ngx-filter-pipe';
 import { DialogPadronAcuicolaComponent } from 'src/app/dialog/dialog-padron-acuicola/dialog-padron-acuicola.component';
 import { CrudService } from 'src/app/services/crud/crud.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-padron-acuicola',
@@ -10,29 +12,88 @@ import { CrudService } from 'src/app/services/crud/crud.service';
 })
 export class PadronAcuicolaComponent implements OnInit {
 
-  constructor(public _dialog: MatDialog,
-              public service: CrudService) { }
-
+  constructor( public _dialog: MatDialog, public crud: CrudService, private filterPipe: FilterPipe,) { 
+    console.log(filterPipe.transform(this.data, { oasa: ''}));
+  }
+  response:     any   = {};
   data: any [];
+  dataFilter: any = { oasa: '' };  
+  p: number = 1;
+
+  
   ngOnInit(): void {
-    this.service.get('getPadronAcuicola').then(r => {
-      console.log(r);
-      let a:any = r;
-      if(a.success){
-        this.data = a.data;
+    
+    this.crud.get('getPadronAcuicola').then(m => {
+      this.response = m;
+      console.log(this.response);
+      if(this.response.success){
+        this.data = this.response.data;
       }
       
     })
   }
 
-  opendialog(){
-    const dialogRef = this._dialog.open(DialogPadronAcuicolaComponent, {
-      width: "60%"
+  key: string = 'id';
+  reverse: boolean = false;
+  sort (key) {
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+
+  opendialog(item){
+    if(item == null){
+      item = {
+        id_padron_acuicola : 0
+      }
+    }
+    const dialogRef = this._dialog.open(DialogPadronAcuicolaComponent
+      , {
+        data: item,
+        width: "60%"
+      
     });
 
     dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+      });
       
-    });
   }
-
+  delete(item){
+    Swal.fire({
+      title: 'Estas a punto de eliminar',
+      text: "Estas seguro de eliminar "+item.nombre,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.crud.post('delete_padron_acuicola',item).then(r => {
+          console.log(r)
+          let response:any = r;
+            if(response.success){
+              Swal.fire({
+                icon: "success",
+                title: 'Exito',
+                text: response.message
+              })
+              this.ngOnInit();
+          }else{
+            Swal.fire({
+              icon: "error",
+              title: 'Error',
+              text: response.message
+            })
+          }
+        })
+      }
+    })
+    
+    
+  }
 }
+
+
+
